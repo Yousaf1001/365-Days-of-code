1df80b8ab7389e86ef14d3175c220c95c3957ff1
Yousaf1001 <yousimalik1012@gmail.com>
2023-04-22 03:27:28 -0700

    Union problem solution and explaination has added

--- Array/Easy/Union.cpp
+++ Array/Easy/Union.cpp
@@ -1,11 +1,21 @@
- vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+There are only optimle and Brute force Approach to solve  this problem
+1)Optimization solution:
+   
+    For Checking Optimle solution detail visit website
+    website_URL:https://takeuforward.org/data-structure/union-of-two-sorted-arrays/
+
+   #### Time and space complexity 
+   time complexity : O(n+m)
+   space complexity:O(n+m)
+
+   vector<int> findUnion(int arr1[], int arr2[], int n, int m)
     {
         //Your code here
         //return vector with correct order of elements
         vector<int>result;
         if(n==0 && m==0)
           return result;
-        int i,j=0;
+        int i=0,j=0;
         while(i<n && j<m)
         {
             if(arr1[i]<=arr2[j])
@@ -18,7 +28,7 @@
             }
             else 
             {
-                if(result.back()!=arr2[j])
+                if(result.size() == 0 || result.back()!=arr2[j])
                 {
                     result.push_back(arr2[j]);
                 }
@@ -46,3 +56,4 @@
         return result;
         
     }
+};
