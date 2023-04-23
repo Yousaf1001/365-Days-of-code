751123727f054b7c6dd07f31bcc293ef20c9b3f8
Yousaf1001 <yousimalik1012@gmail.com>
2023-04-23 10:41:52 -0700

    Missing Number problem solution and Explaination has added

---
+++ Array/Easy/MissingNumber.cpp
@@ -0,0 +1,4 @@
+There are three  optimle Approaches to solve  this problem
+1)Brute Force Approach: 2)Better Approach:    3)Optimization solution:
+   For Checking Optimle solution detail visit website
+    website_URL:https: https://takeuforward.org/arrays/find-the-missing-number-in-an-array/
